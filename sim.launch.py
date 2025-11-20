789cd86cd012c397171a564e05cedb35329872ff
downwiththepownd <47802041+downwiththepownd@users.noreply.github.com>
2025-11-20 14:58:54 -0500

    Add files via upload

--- sim.launch.py
+++ sim.launch.py
@@ -17,6 +17,10 @@ def generate_launch_description():
             arguments=[
                 # ROS → Gazebo
                 '/model/pendulum_cart/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist',
+                '/world/pendulum_world/model/pendulum_cart/joint/br_wheel/cmd_vel@std_msgs/msg/Float64]gz.msgs.Double',
+                '/world/pendulum_world/model/pendulum_cart/joint/fr_wheel/cmd_vel@std_msgs/msg/Float64]gz.msgs.Double',
+                '/world/pendulum_world/model/pendulum_cart/joint/bl_wheel/cmd_vel@std_msgs/msg/Float64]gz.msgs.Double',
+                '/world/pendulum_world/model/pendulum_cart/joint/fl_wheel/cmd_vel@std_msgs/msg/Float64]gz.msgs.Double',
                 # Gazebo → ROS
                 '/model/pendulum_cart/odometry@nav_msgs/msg/Odometry@gz.msgs.Odometry',
                 '/world/pendulum_world/model/pendulum_cart/joint_state@sensor_msgs/msg/JointState[gz.msgs.Model'
